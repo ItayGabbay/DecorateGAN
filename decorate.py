a8ca1fbdaeeaf7798730b079d3e65d2aac972fff
ItayGabbay <itay240594@gmail.com>
2018-12-31 00:27:40 +0200

    Finish DECORATE

--- decorate.py
+++ decorate.py
@@ -9,63 +9,73 @@ class Decorate:
         self.n_estimators = n_estimators
         self.max_iter = max_iter
         self.r_factor = r_factor
+        self.possible_labels = None
 
     def fit(self, features, labels):
         iter_num = 1
         ensemble_size = 1
-        first_model = DecisionTreeClassifier()
+        self.trees = []
+        first_model = DecisionTreeClassifier(class_weight='balanced', max_depth=9)
         first_model.fit(features, labels)
         self.trees.append(first_model)
+        self.possible_labels = labels.unique()
         current_error = self._compute_ensemble_error(features, labels)
+        print("Current Error", current_error)
         while ensemble_size < self.n_estimators and iter_num < self.max_iter:
+            print("Iteration number", iter_num)
             artificial_data = self._generate_artificial_data(features)
             artificial_labels = self._label_artificial_data(artificial_data)
-            training_data = pd.concat([features, artificial_data], axis=1)
-            training_labels = pd.concat([labels, artificial_labels], axis=1)
-
-            new_model = DecisionTreeClassifier()
+            training_data = pd.concat([features, artificial_data], axis=0)
+            training_labels = np.concatenate((labels.values, artificial_labels), axis=0)
+            new_model = DecisionTreeClassifier(class_weight='balanced', max_depth=9)
             new_model.fit(training_data, training_labels)
             self.trees.append(new_model)
             error = self._compute_ensemble_error(features, labels)
+            print("with new error", error)
             if error < current_error:
                 current_error = error
                 ensemble_size += 1
+                print("Appending classifier ", ensemble_size)
             else:
                 self.trees.remove(new_model)
             iter_num += 1
 
     def predict(self, samples):
-        results = np.zeros(len(samples))
-        ensemble_size = len(self.trees)
-        num_of_labels = len(self.trees[0].predict_proba(samples[0]))
-        for sample_i in range(len(samples)):
-            probas = np.zeros(num_of_labels)
-            for tree in self.trees:
-                probas += tree.predict_proba(samples[sample_i]) / ensemble_size
-            results[sample_i] = probas.index(max(probas))
+        results = np.zeros(samples.shape[0])
+        probas = self.predict_proba(samples)
+        for sample_i in samples:
+            results[sample_i] = probas[sample_i].index(max(probas[sample_i]))
         return results
 
-    def _compute_ensemble_error(self, features, labels):
-        probas = np.zeros((len(features), len(labels.unique())))
+    def predict_proba(self, samples):
         ensemble_size = len(self.trees)
-        for feature_i in range(len(features)):
-            for tree in self.trees:
-                probas[feature_i] += tree.predict_proba(features[feature_i]) / ensemble_size
+        num_of_labels = len(self.possible_labels)
+        probas = np.zeros((samples.shape[0], num_of_labels))
+        for tree in self.trees:
+            probas += tree.predict_proba(samples) / ensemble_size
+
+        return probas
+
+    def _compute_ensemble_error(self, samples, labels):
+        probas = np.zeros((len(samples), len(labels.unique())))
+        ensemble_size = len(self.trees)
+        for tree in self.trees:
+            probas += tree.predict_proba(samples) / ensemble_size
 
         errors = 0
         for proba_i in range(len(probas)):
-            predicted = probas[proba_i].index(max(probas[proba_i]))
+            predicted = np.argmax(probas[proba_i])
             if predicted != labels[proba_i]:
                 errors += 1
 
-        return errors / len(features)
+        return errors / len(samples)
 
     def _generate_artificial_data(self, features : pd.DataFrame):
-        artificial_data = np.empty(features.shape)
         number_of_samples = int(features.shape[0] * self.r_factor)
+        artificial_data = np.empty((number_of_samples, features.shape[1]))
         for col_index in range(len(features.columns)):
             column = features[features.columns[col_index]]
-            if column.dtype == np.numeric:
+            if column.dtype == np.number:
                 artificial_data[:, col_index] = np.random.normal(np.mean(column),
                                                                  np.std(column),
                                                                  size=number_of_samples)
@@ -76,11 +86,27 @@ class Decorate:
                                                                       unq_classes,
                                                                       size=number_of_samples)
         artificial_data = pd.DataFrame(artificial_data, columns=features.columns)
-
         return artificial_data
 
     def _label_artificial_data(self, artificial_data):
-        #TODO: Label the artificial data correctly
-        return artificial_data
+        labels = np.zeros(artificial_data.shape[0])
+        probabilities = self.predict_proba(artificial_data)
 
+        for sample in range(len(probabilities)):
+            normalized_probas = self._normalize_probas(probabilities[sample])
+            for prob_index in range(len(normalized_probas)):
+                normalized_probas[prob_index] = 1 / normalized_probas[prob_index]
+
+                normalized_probas = normalized_probas / np.sum(normalized_probas)
+            labels[sample] = np.random.choice(self.possible_labels, p=normalized_probas)
+        return labels
+
+
+    def _normalize_probas(self, probas):
+        for proba in probas:
+            if proba == 0:
+                proba = 0.001
+
+        sum_probas = np.sum(probas)
+        return probas / sum_probas
 
